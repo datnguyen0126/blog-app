b6a2bbf6fe6e042c64f6a47926107dd873a4311b
datnguyen <nguyenhuudat0165@gmail.com>
2020-07-20 15:38:31 +0700

    fix conflict

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,9 @@ urlpatterns = [
     path("register/", views.register, name="blog-register"),    
     path('login/', views.login, name='blog-login'),
     path('logout/', views.logout, name='blog-logout'),
+    path('blog/create/', views.blog_create, name='blog-create-post'),
+    path('blog/edit/<int:id>/', views.blog_create, name='blog-update-post'),
+    path('blog/delete/<int:id>/', views.blog_delete, name='blog-delete-post'),
+    path('about/', views.about, name='blog-about'),
     path('', views.home, name='blog-home'),
 ]
-
